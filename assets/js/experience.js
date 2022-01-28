e3b4b07c1c60be9df1f189278afce6dd2dfa1ced
Shubham Singh <73239204+Shubhhaamsingh@users.noreply.github.com>
2022-01-28 20:03:42 +0530

    My Personal Website.

---
+++ assets/js/experience.js
@@ -0,0 +1,122 @@
+AOS.init();
+
+//  Work experience cards
+
+
+const experiencecards = document.querySelector(".experience-cards");
+const exp = [
+  {
+    title: "Teaching Assistant Intern",
+    cardImage: "assets/images/experience-page/coding.png",
+    place: "Coding Ninjas",
+    time: "(Aug, 2021 - Oct, 2021)",
+    desp: "<li>Taking doubt sessions.</li> <li>Debugging codes.</li> <li>Helping students in their assignments & various projects.</li>",
+  },
+   
+   
+];
+
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+    <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
+      <div class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </div>
+    </div>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
+
+
+// Volunteership Cards
+
+const volunteership = document.querySelector(".volunteership");
+const volunteershipcards = [
+  {
+    title: "EOSINT",
+    cardImage: "assets/images/experience-page/Email-Testing-Tools.png",
+    description:
+      "Email open source intelligence for check E-mail Verification, Breached Leaks Website, Breached Data.",
+  },
+  {
+    title: "Sub Domain OSINT",
+    cardImage: "assets/images/experience-page/qq.png",
+    description:
+      "An Open source intelligence tool to scan subdomains of any website." },
+   
+];
+
+const showCards = () => {
+  let output = "";
+  volunteershipcards.forEach(
+    ({ title, cardImage, description }) =>
+      (output += `        
+      <div class="card volunteerCard" data-aos="fade-down" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="600" style="height: 550px;width:400px">
+      
+      <img src="${cardImage}" height="250" width="65" class="card-img" style="border-radius:10px">
+      <div class="content">
+          <h2 class="volunteerTitle">${title}</h2><br>
+          <p class="copy">${description}</p></div>
+      
+      </div>
+      `)
+  );
+  volunteership.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+
+// Hackathon Section
+
+
+const hackathonsection = document.querySelector(".hackathon-section");
+const mentor = [
+  {
+    title: "Hack-A-Solution",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/hackasolution.png",
+    desp: "Hack-a-Solution 2021 is a 24 hour long hackathon organized by Frisco students to raise awareness of world issues through technology.",
+  },
+   
+];
+
+const showCards3 = () => {
+  let output = "";
+  mentor.forEach(
+    ({ title, image, subtitle, desp}) =>
+      (output += `  
+      <div class="blog-slider__item swiper-slide">
+        <div class="blog-slider__img">
+            <img src="${image}" alt="">
+        </div>
+        <div class="blog-slider__content">
+          <div class="blog-slider__title">${title}</div>
+          <span class="blog-slider__code">${subtitle}</span>
+          <div class="blog-slider__text">${desp}</div>
+          <a href="#" class="blog-slider__button">Read More</a>   
+        </div>
+      </div>
+      `)
+  );
+  hackathonsection.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards3);
